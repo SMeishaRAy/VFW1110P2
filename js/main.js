7e515d0bab0b857cb972b13c2bca86b50b16d155
S. Meisha Ray <bluedragoness@fullsail.edu>
2011-10-01 15:09:09 -0400

    Added a new function that doesn't work.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 	Term: VFW1110
 	Created: September 30, 2011
 */
+
 //DOM - method when the dom has loaded it will run this function.
 window.addEventListener("DOMContentLoaded", function(){
 	//get element by id function.
@@ -11,28 +12,59 @@ window.addEventListener("DOMContentLoaded", function(){
 				return theElement;
 }
 
-	var showList = [" --Choose a Show--", "", "Chevy", "Corvette", "Camaro", "Ford", "Mopar", "Tuner", "Antique", "Classic", "Low-Riders", "Lifted-Rides"];
+	//create select field element and populate options.
+		functions makeShows(){
+			var formTag = document.getElementsByTagName("form"),//formTag is an array of all form tags.
+				selectLi = $("shows"),
+				makeSelect = document.createElement('select');
+				makeSelect.setAttribute("id", "groups");
+			for(var i = 0, j = showList.length; i<j; i++){
+				var makeOption = document.createElement('option');
+					var optText = showList[i];
+					makeOption.setAttribute("value", optText);
+					makeOption.innerHTML = optText;
+					makeSelect.appendChild(makeOption);
+		}
+			selectLi.appendChild(makeSelect);
+}
+
+	function storeData(){
+		var id 		= Math.floor(Math.radom()*10000001);
+			//gather up all of form field varlues and store in an object.
+			//object properties contain array with the form label and input values.
+			var item		= {};
+				item.group 	= ["Group:", $('groups').value];
+				item.fname  = ["First Name:", $('fname').value];
+				item.lname  = ["Last Name:", $('lname').value];
+				item.email  = ["Email:", $('email').value];
+				item.sex	= ["Sex:", $('sex').value];
+				
+}
+
+	//array
+	var showList = ["--Choose One--", "Chevy", "Corvette", "Camaro", "Ford", "Mopar", "Tuner", "Antique", "Classic", "Low-Riders", "Lifted-Rides"];
+	makeShows();
 	
 	// Set Link & Submit Click 
-		var displayLink = $("displayLink");
-			displayLink.addEventLister("click", getData);
+		/*var displayLink = $("displayLink");
+			displayLink.addEventListener("click", getData);
 		var clearLink = $("clear");
-			clearLink.addEventLister("click", clearLocal);
+			clearLink.addEventListener("click", clearLocal);*/
+		//Save Data function
 		var save = $("submit");
-			save.addEventLister("click", storeData);
-//Need Save Data function
-
-//Need Save a Key(randomNumber) for values (array or object). Each saved entry will be a key:value pair.
-
-//The Key should be a random number wich will create a unique id.
-
-//The value should be an array or object that cantains the values of your form field data.
-
-//This allows for you to save multiple submissions from you form into local storage without overwriting dada.
-
-//Use web inspector to test local data storage.
-
-//JS: Get data 
-//JS: Array Function - write an array function(s) that populate atleast one select form element.  (dropdown menu).
+			save.addEventListener("click", storeData);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
